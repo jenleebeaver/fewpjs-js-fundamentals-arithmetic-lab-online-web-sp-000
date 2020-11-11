e5c29c09b988951ac91eb1739adf5922b3bd6b86
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-11 00:27:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 // Write your code here
 
 // 1. Create a variable called multiply that will multiply the variables num1 and num2 whose results will equal "62".
-const var1 = 2
-const var2 = 31
+const num1 = 2
+const num2 = 31
 var multiply = num1 * num2
